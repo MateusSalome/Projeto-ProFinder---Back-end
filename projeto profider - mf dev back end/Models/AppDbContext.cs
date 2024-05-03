0e66ea31286b8bbda8318ef9a909bd998b39a3f1
mateus.salome@sga.pucminas.br <1494220@sga.pucminas.br>
2024-05-03 15:48:50 -0300

    Configuração do banco de dados e da tabela veiculo

---
+++ projeto profider - mf dev back end/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_profider___mf_dev_back_end.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
